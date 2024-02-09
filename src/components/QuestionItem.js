c0ebdeebcc8f46d83506b1ba368dc06e9ae8fcdd
Deborah Kurz <DeborahKurz6@gmail.com>
2024-02-09 12:51:48 -0700

    Built out GET, POST, & DELETE requests

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -3,6 +3,14 @@ import React from "react";
 function QuestionItem({ question }) {
   const { id, prompt, answers, correctIndex } = question;
 
+  function handleDelete(){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+    .then(()=>console.log("deleted"))
+    .catch((error)=>console.log(error("Error deleting question: ", error)))
+  }
+
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
@@ -17,7 +25,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
